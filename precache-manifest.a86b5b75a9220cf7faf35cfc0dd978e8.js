ed90a19d5ba992aa658730c51af44968b520874f
kevinwalsh23 <kevinwalsh23@gmail.com>
2019-07-12 22:19:27 -0400

    Updates

---
+++ precache-manifest.a86b5b75a9220cf7faf35cfc0dd978e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffeaf1b6e34dda209bb24400bd416504",
+    "url": "/vidomate/index.html"
+  },
+  {
+    "revision": "d0573a497ecd10a991c5",
+    "url": "/vidomate/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/vidomate/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "d0573a497ecd10a991c5",
+    "url": "/vidomate/static/js/main.10cf5a98.chunk.js"
+  },
+  {
+    "revision": "02a8d13010a0bee498d0",
+    "url": "/vidomate/static/js/runtime~main.3bd9f31c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/vidomate/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fa2dfd2d4c094307f14b3c87ec6fb2a2.js"
+  "/vidomate/precache-manifest.a86b5b75a9220cf7faf35cfc0dd978e8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vidomate/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
